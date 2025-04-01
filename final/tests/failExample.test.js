3e2c427d67d2549204fbda80ea5a92190a53e330
unknown <michaeladdis78@gmail.com>
2025-04-01 21:20:44 +0300

    fix bug

--- final/tests/failExample.test.js
+++ final/tests/failExample.test.js
@@ -1,5 +1,5 @@
-const createJiraIssue = require('../utils/jiraReporter');
-
+// const createJiraIssue = require('../utils/jiraReporter');
+import createJiraIssue from '../utils/jiraReporter';
 test('Intentional fail to test Jira integration', async () => {
   try {
     expect(1).toBe(2); // This will fail
